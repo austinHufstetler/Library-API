6fc2517a6320c13a823396d73d77d35fe271590b
Austin Hufstetler <austinhufstetler@gmail.com>
2017-09-11 18:24:48 -0400

    Create BookManagement.java

---
+++ code/BookManagement.java
@@ -0,0 +1,18 @@
+public class BookManagement {
+
+	public void add(Book book) {
+	
+	}
+	public void read(Book book) {
+	
+	}
+  
+  public void update(Book book) {
+
+	}
+	public void delete(Book book) {
+
+  }
+
+	
+}
