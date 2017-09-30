a2a64980edf63ab7d38352081b6fcf54c177787e
Austin Hufstetler <austinhufstetler@gmail.com>
2017-09-29 21:38:14 -0400

    Update Member.java

--- code/Library/src/users/Member.java
+++ code/Library/src/users/Member.java
@@ -2,12 +2,11 @@ package users;
 
 public class Member extends User {
 
-	public Member(String firstName, String lastName, String username, String password){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.role = "member";
-		this.username = username;
-		this.password = password;
+	public Member(String firstName, String lastName, String role, String username, String password) {
+		super(firstName, lastName, role, username, password);
+		// TODO Auto-generated constructor stub
 	}
+
+
 	
 }
