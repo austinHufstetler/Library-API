7953cfadf975b1e8d71c60d8e228a144a181f474
Austin Hufstetler <austinhufstetler@gmail.com>
2017-09-30 16:54:55 -0400

    changes with users

--- code/Library/src/users/Manager.java
+++ code/Library/src/users/Manager.java
@@ -1,9 +1,9 @@
 package users;
 
 public class Manager extends Employee {
-
-	public Manager(String firstName, String lastName, String role, String username, String password) {
-		super(firstName, lastName, role, username, password);
+	
+	public Manager(String firstName, String lastName, String username, String password) {
+		super(firstName, lastName, "manager", username, password);
 		// TODO Auto-generated constructor stub
 	}
 	
