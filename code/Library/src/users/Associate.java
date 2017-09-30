b83c78a26b79923823619b315598fa5068477046
Austin Hufstetler <austinhufstetler@gmail.com>
2017-09-29 21:37:14 -0400

    Update Associate.java

--- code/Library/src/users/Associate.java
+++ code/Library/src/users/Associate.java
@@ -2,12 +2,11 @@ package users;
 
 public class Associate extends Employee {
 
-	public Associate(String firstName, String lastName, String username, String password){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.role = "associate";
-		this.username = username;
-		this.password = password;
+	public Associate(String firstName, String lastName, String role, String username, String password) {
+		super(firstName, lastName, role, username, password);
+		// TODO Auto-generated constructor stub
 	}
+
+
 
 }
